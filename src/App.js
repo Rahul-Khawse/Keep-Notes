85fc6fad022282339cf670c0fcbc6baf159f0718
Rahul khawse <120732862+RahulKhawse@users.noreply.github.com>
2024-01-03 10:06:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="form">
+        <Stack direction="row" spacing={2}>
+          <TextField 
+            value={name}
+            onChange={(element) => setName(element.target.value)}
+            id="outlined-basic" 
+            label="Name" 
+            variant="outlined" 
+          />
+          <TextField 
+            value={email}
+            onChange={(element) => setEmail(element.target.value)}
+            id="outlined-basic" 
+            label="Email" 
+            variant="outlined"
+          />
+          <Button 
+            variant="contained" 
+            color="success">
+            <AddIcon />
+          </Button>
+        </Stack>
+      </div>
     </div>
   );
 }
